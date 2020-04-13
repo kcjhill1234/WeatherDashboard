35d344c9a1dc65caf510abd01f98270e9db786a8
ktotheH <kcjhill1234@msn.com>
2020-04-12 21:17:55 -0600

    made UI

---
+++ Assets/script.js
@@ -0,0 +1,5 @@
+// api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
+
+// api.openweathermap.org/data/2.5/forecast?q={city name}&appid={your api key}
+
+// 08a56e31985e47f9e872bd542c94288a
